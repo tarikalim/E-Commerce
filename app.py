ec02ca72339cffad361f42a207dc2909b02bc5c5
tarikalim <tarikalim1899@gmail.com>
2024-02-11 16:44:32 +0300

    some fixes.

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
+# app.py
 from flask import Flask
 from models import db
-from controllers import init_app_routes
+import routes
 
-
-def create_app():
-    app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://tarik@34.141.227.97/E-Commerce'
-    db.init_app(app)
-    init_app_routes(app)
-
-    return app
-
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://tarik@34.141.227.97/E-Commerce'
+db.init_app(app)
+routes.init_routes(app)
 
 if __name__ == '__main__':
-    app = create_app()
     with app.app_context():
         db.create_all()
     app.run(debug=True)
--- controllers.py
+++ controllers.py
@@ -1,28 +1,26 @@
-from flask import jsonify, request
-from models import *
+# controllers.py
+from models import User, db
 from sqlalchemy.exc import IntegrityError
+from flask import jsonify, request
 
 
-def init_app_routes(app):
-    @app.route('/register', methods=['POST'])
-    def register():
-        try:
-            data = request.get_json()
-            new_user = User(Username=data['username'], Password=data['password'], Email=data.get('email'),
-                            Address=data.get('address'))
-            db.session.add(new_user)
-            db.session.commit()
-            return jsonify({"message": "Register ok"}), 201
-
-        except IntegrityError:
-            db.session.rollback()
-            return jsonify({"message": "Invalid username"}), 400
-
-    @app.route('/login', methods=['POST'])
-    def login():
+def register_user():
+    try:
         data = request.get_json()
-        user = User.query.filter_by(Username=data['username']).first()
-        if user and user.Password == data['password']:
-            return jsonify({"message": "Login ok", "user_id": user.UserID}), 200
-        else:
-            return jsonify({"message": "Invalid"}), 401
+        new_user = User(Username=data['username'], Password=data['password'], Email=data.get('email'),
+                        Address=data.get('address'))
+        db.session.add(new_user)
+        db.session.commit()
+        return jsonify({"message": "Register ok"}), 201
+    except IntegrityError:
+        db.session.rollback()
+        return jsonify({"message": "Username already exist"}), 400
+
+
+def login_user():
+    data = request.get_json()
+    user = User.query.filter_by(Username=data['username']).first()
+    if user and user.Password == data['password']:
+        return jsonify({"message": "Login ok", "user_id": user.UserID}), 200
+    else:
+        return jsonify({"message": "Invalid username or password"}), 401
