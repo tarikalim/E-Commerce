08ad9e718403ab2d9d0786dee5897877e322270b
tarikalim <tarikalim1899@gmail.com>
2024-02-15 03:00:44 +0300

    admin token function added

--- middleware.py
+++ middleware.py
@@ -1,11 +1,11 @@
 from functools import wraps
 from flask import request, jsonify
 import jwt
-from models import User
+from models import User, Admin
 from creates_app import creates_app
 
 
-def token_required(f):
+def user_token_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         token = None
@@ -25,3 +25,27 @@ def token_required(f):
         return f(current_user, *args, **kwargs)
 
     return decorated_function
+
+
+def admin_token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'Authorization' in request.headers:
+            token = request.headers['Authorization'].split(" ")[1]
+
+        if not token:
+            return jsonify({'message': 'Token is missing!'}), 401
+
+        try:
+            data = jwt.decode(token, creates_app().config['SECRET_KEY'], algorithms=["HS256"])
+            admin = Admin.query.filter_by(AdminID=data['admin_id']).first()
+            if not admin:
+                return jsonify({'message': 'Token is invalid!'}), 401
+        except:
+            return jsonify({'message': 'Token is invalid!'}), 401
+
+        return f(admin, *args, **kwargs)
+
+    return decorated
