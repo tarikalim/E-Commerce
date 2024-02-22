7a681fbfad4f0eabe1fc5edddc90cc4ecd2948b8
tarikalim <tarikalim1899@gmail.com>
2024-02-22 23:08:49 +0300

    check the mail domain has MX record or not.

---
+++ Middlewares/mailDomainCheck.py
@@ -0,0 +1,13 @@
+import dns.resolver
+
+
+def check_mx_record(email):
+    domain = email.split('@')[-1]
+    try:
+        dns.resolver.resolve(domain, 'MX')
+        return True
+    except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN):
+        return False
+
+
+
