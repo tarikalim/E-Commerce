df145792411c587c67c57909dc27037b67e32f9d
tarikalim <tarikalim1899@gmail.com>
2024-02-22 21:54:16 +0300

    extra security for passwords and other user informations using regex.

--- Controller/user.py
+++ Controller/user.py
@@ -4,12 +4,29 @@ from Controller.common import *
 def register_user():
     try:
         data = request.get_json()
-        hashed_password = generate_password_hash(data['password'])
-        new_user = User(Username=data['username'], Password=hashed_password,
-                        Email=data.get('email'), Address=data.get('address'))
+        username = data['username']
+        password = data['password']
+        email = data.get('email')
+        address = data.get('address')
+
+        if len(password) < 8 or not re.search("[a-zA-Z]", password) or not re.search("[0-9]", password):
+            return jsonify({
+                'message': 'Password must be at least 8 characters long and include at least one '
+                           'letter and one number'}), 400
+
+        email_regex = r'^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+        if not re.match(email_regex, email):
+            return jsonify({'message': 'Invalid email format'}), 400
+
+        if User.query.filter_by(Email=email).first():
+            return jsonify({'message': 'Email already exists'}), 400
+
+        hashed_password = generate_password_hash(password)
+        new_user = User(Username=username, Password=hashed_password, Email=email, Address=address)
         db.session.add(new_user)
         db.session.commit()
         return jsonify({"message": "Register ok"}), 201
+
     except IntegrityError:
         db.session.rollback()
         return jsonify({"message": "Username already exist"}), 400
@@ -32,7 +49,8 @@ def login_user():
 
 
 def get_user_info(current_user):
-    user_data = {'username': current_user.Username, 'email': current_user.Email, 'address': current_user.Address}
+    user_data = {'username': current_user.Username, 'email': current_user.Email, 'address': current_user.Address,
+                 'credit card id': current_user.CreditCardID}
     return jsonify(user_data), 200
 
 
@@ -40,18 +58,29 @@ def update_user(current_user):
     data = request.get_json()
     user = User.query.filter_by(UserID=current_user.UserID).first()
 
-    if not user:
-        return jsonify({'message': 'User not found'}), 404
-
     if 'username' in data:
+        if User.query.filter(User.Username == data['username'], User.UserID != current_user.UserID).first():
+            return jsonify({'message': 'Username already taken'}), 400
         user.Username = data['username']
+
     if 'email' in data:
+        email_regex = r'^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+        if not re.match(email_regex, data['email']):
+            return jsonify({'message': 'Invalid email format'}), 400
+        if User.query.filter(User.Email == data['email'], User.UserID != current_user.UserID).first():
+            return jsonify({'message': 'Email already exists'}), 400
         user.Email = data['email']
+
     if 'address' in data:
         user.Address = data['address']
+
     if 'creditcardID' in data:
+        credit_card_regex = r'^\d{16}$'
+        if not re.match(credit_card_regex, data['creditcardID']):
+            return jsonify({
+                'message': 'Invalid credit card number, credit card '
+                           'number must be 16 digit and contain only numbers.'}), 400
         user.CreditCardID = data['creditcardID']
 
     db.session.commit()
-
     return jsonify({'message': 'User updated successfully'}), 200
