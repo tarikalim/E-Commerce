c746286a69de935a51d2ef520d72cce9dc6b637f
tarikalim <tarikalim1899@gmail.com>
2024-02-24 22:09:33 +0300

    .

--- Controller/common.py
+++ Controller/common.py
@@ -1,10 +1,11 @@
 import datetime
 import jwt
+import re
 from flask import jsonify, request
 from sqlalchemy.exc import IntegrityError
 from creates_app import creates_app
 from Model.models import *
 from datetime import date
 from werkzeug.security import generate_password_hash, check_password_hash
-from Middlewares.mailDomainCheck import check_mx_record
-import re
+from helper.validation.mailCheck import validate_email
+from helper.validation.creditCardCheck import validate_credit_card
