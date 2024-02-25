1d443c163d2299114f24a3a413f0e3d78b255aea
tarikalim <tarikalim1899@gmail.com>
2024-02-25 12:59:48 +0300

    import statements

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from creates_app import creates_app
-from Model.models import db
 from Route import routes
+# from extensions import db
 
 app = creates_app()
 routes.init_routes(app)
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+    # with app.app_context(): //activate those lines to create db
+    #     db.create_all()
 
     app.run(debug=True)
--- creates_app.py
+++ creates_app.py
@@ -1,15 +1,15 @@
 from flask import Flask
-from flask_jwt_extended import JWTManager
-from Model.models import db
+from extensions import *
+from config import Config
 
 
 def creates_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
 
-    app.config['SECRET_KEY'] = 'deneme123'
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
-
-    JWTManager(app)
+    mail.init_app(app)
     db.init_app(app)
+    jwt.init_app(app)
+    migrate.init_app(app, db)
 
     return app
