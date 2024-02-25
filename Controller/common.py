8b49d91d6fe004e7df96249387afaefe700162a2
tarikalim <tarikalim1899@gmail.com>
2024-02-25 12:59:30 +0300

    .

--- Controller/common.py
+++ Controller/common.py
@@ -7,5 +7,8 @@ from creates_app import creates_app
 from Model.models import *
 from datetime import date
 from werkzeug.security import generate_password_hash, check_password_hash
-from helper.validation.mailCheck import validate_email
-from helper.validation.creditCardCheck import validate_credit_card
+from helper.mailCheck import validate_email
+from helper.creditCardCheck import validate_credit_card
+from helper.passwordCheck import validate_password
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired, BadSignature
+from helper.sendMail import send_email
