01f365e8eb9f7d431f6eb27c2f49773dbf98c7a2
tarikalim <tarikalim1899@gmail.com>
2024-03-22 12:20:35 +0300

    import dotenv to pass .env files to app

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
+from dotenv import load_dotenv
+load_dotenv()
 from create_app import create_app
-# from extensions import db
+from extensions import db
+
 
 app = create_app()
 
 if __name__ == '__main__':
-    # with app.app_context(): //activate those lines to create db
-    #     db.create_all()
+    with app.app_context():
+        db.create_all()
 
     app.run(debug=True)
